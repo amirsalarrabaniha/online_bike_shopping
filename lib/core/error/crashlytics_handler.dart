6d7d46e07f176ea7c7d40f3945c6974d19ee28c3
ami <amyrsalarr@gmail.com>
2025-08-25 22:47:02 +0330

    chores: remove firebase

--- lib/core/error/crashlytics_handler.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
-
-void setupCrashlytics() {
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-}
